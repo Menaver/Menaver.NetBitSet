cd08138d913362334f520695bf7dffc68fa7a06e
Menaver <menaver@outlook.com>
2024-05-15 12:15:34 +0200

    add peft tests launch profiles (#22)
    Co-authored-by: Dima Kileyko <dmitrykileyko@coherentsolutions.com>

--- src/Menaver.NetBitSet.Tests.Perf/Program.cs
+++ src/Menaver.NetBitSet.Tests.Perf/Program.cs
@@ -4,8 +4,20 @@ namespace Menaver.NetBitSet.Tests.Perf;
 
 public class Program
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-        BenchmarkRunner.Run(typeof(Program).Assembly);
+        if (args.Any())
+        {
+            var types = typeof(Program).Assembly.GetTypes().Where(type => args.Contains(type.Name)).ToList();
+
+            foreach (var type in types)
+            {
+                BenchmarkRunner.Run(type);
+            }
+        }
+        else
+        {
+            BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
     }
 }
